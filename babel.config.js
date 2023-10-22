f3f6225e3356d4ed5146678ba22bddd71216655f
Tommy <86270372+tommerty@users.noreply.github.com>
2023-10-22 16:13:40 +0100

    gluestick

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      // Required for expo-router
+      'expo-router/babel',
+    ],
   };
 };
