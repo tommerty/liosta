b3f2209dd14304cee1cec9a1171dddc98ab3c39f
Tommy <86270372+tommerty@users.noreply.github.com>
2023-10-24 15:57:05 +0100

    settings tab bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,71 @@
-import { Stack } from 'expo-router';
-import { Link } from '@react-navigation/native';
-import { AddIcon, Button, ButtonIcon, InfoIcon } from '@gluestack-ui/themed';
+import { Tabs } from 'expo-router/tabs';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-export default function Layout() {
+export default function AppLayout() {
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#2a204b',
-                },
-                title: 'Liosta',
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                },
-                headerRight: () => (
-                    <Link to="/about" style={{ color: '#fff', marginRight: 10 }}>
-                        <ButtonIcon as={InfoIcon} color='#fff' />
-                    </Link>
-                ),
-            }}
-        />
+        <Tabs screenOptions={{
+
+        }}>
+            <Tabs.Screen
+                // Name of the route to hide.
+                name="index"
+                options={{
+                    // This tab will no longer show up in the tab bar.
+                    title: 'Home',
+                    headerTitle: 'Liosta',
+                    tabBarLabel: 'Home',
+                    tabBarLabelStyle: {
+                        color: 'white',
+                    },
+                    headerStyle: {
+                        backgroundColor: '#2a204b',
+                        shadowColor: 'transparent',
+                    },
+                    headerTitleStyle: {
+                        color: 'white',
+                        fontSize: 24,
+                    },
+                    tabBarActiveBackgroundColor: '#413370',
+                    tabBarInactiveBackgroundColor: '#2b224a',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="home" color={"white"} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                // Name of the route to hide.
+                name="settings"
+                options={{
+                    // This tab will no longer show up in the tab bar.
+                    title: 'Settings',
+                    headerTitle: 'Settings',
+                    tabBarLabel: 'Settings',
+                    tabBarLabelStyle: {
+                        color: 'white',
+                    },
+                    headerStyle: {
+                        backgroundColor: '#2a204b',
+                        shadowColor: 'transparent',
+                    },
+                    headerTitleStyle: {
+                        color: 'white',
+                        fontSize: 24,
+                    },
+                    tabBarActiveBackgroundColor: '#413370',
+                    tabBarInactiveBackgroundColor: '#2b224a',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="cog" color={"white"} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                // Name of the route to hide.
+                name="about"
+                options={{
+                    // This tab will no longer show up in the tab bar.
+                    href: null
+                }}
+            />
+        </Tabs>
     );
 }
